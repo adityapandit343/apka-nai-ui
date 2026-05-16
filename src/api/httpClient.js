6c89fa6d15bf4ff08132cb7a17a281d17f0292da
adityapandit343 <adityadimri343@gmail.com>
2026-05-16 16:35:14 +0530

    push data

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -22,7 +22,7 @@ apiClient.interceptors.request.use((config) => {
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 || error.response?.status === 403) {
       clearAuthSession();
       window.dispatchEvent(new CustomEvent('cutbook:unauthorized'));
 
